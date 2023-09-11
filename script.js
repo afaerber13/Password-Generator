7e26ed47d7cb665696a02e80b00ce7cf6dd0b5d0
Alyssa Faerber <alyssa.faerber@gmail.com>
2023-09-11 13:54:34 -0600

    adding variables for characters, numbers, and letters.

--- script.js
+++ script.js
@@ -8,8 +8,21 @@ var length = Number(prompt("How many characters will your password be? Choose be
 
 
 character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+space = [];
+var choices;
+var toUpper = function (x) {
+  return x.toUpperCase();
+};
+alphaUpper = alpha.map(toUpper);
 
+var get = document.querySelector("#generate");
 
+get.addEventListener("click", function () {
+    ps = generatePassword();
+    document.getElementById("password").placeholder = ps;
+});
 
 
 function generatePassword() {
